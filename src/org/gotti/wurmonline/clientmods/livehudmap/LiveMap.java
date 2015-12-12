83ea532c43a4998b53b523ae3c5b05a49558ac26
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-12-12 18:57:10 +0100

    Only update the map on movement or terrain changes

--- src/org/gotti/wurmonline/clientmods/livehudmap/LiveMap.java
+++ src/org/gotti/wurmonline/clientmods/livehudmap/LiveMap.java
@@ -8,11 +8,13 @@ import org.gotti.wurmonline.clientmods.livehudmap.renderer.MapRendererFlat;
 import org.lwjgl.opengl.GL11;
 
 import com.wurmonline.client.game.PlayerPosition;
+import com.wurmonline.client.game.TerrainChangeListener;
 import com.wurmonline.client.game.World;
+import com.wurmonline.client.renderer.cave.CaveBufferChangeListener;
 import com.wurmonline.client.resources.textures.ImageTexture;
 import com.wurmonline.client.resources.textures.ImageTextureLoader;
 
-public class LiveMap {
+public class LiveMap implements TerrainChangeListener, CaveBufferChangeListener {
 
 	private int size;
 	private int mapsize;
@@ -29,6 +31,8 @@ public class LiveMap {
 	private int y;
 	private int cavemapsize;
 	
+	private int px = 0, py = 0;
+	
 	public LiveMap(World world, int size, int mapsize) {
 		this.size = size;
 		this.mapsize = mapsize;
@@ -36,19 +40,27 @@ public class LiveMap {
 		this.world = world;
 		this.surfaceRenderer = new MapRendererFlat(world.getNearTerrainBuffer(), 0x1000, 0x1000, this.mapsize);
 		this.caveRenderer = new MapRendererCave(world.getCaveBuffer(), 0x1000, 0x1000, this.cavemapsize);
+		
+		this.world.getNearTerrainBuffer().addListener(this);
+		this.world.getCaveBuffer().addCaveBufferListener(this);
 	}
 
 	public void update(int windowX, int windowY) {
 		x = windowX + 3;
 		y = windowY + 21;
-		if (dirty) {
-			PlayerPosition pos = world.getPlayer().getPos();
+
+		PlayerPosition pos = world.getPlayer().getPos();
+		if (dirty || px != pos.getTileX() || py != pos.getTileY()) {
+			px = pos.getTileX();
+			py = pos.getTileY();
 			
 			if (pos.getLayer() >= 0)
-				image = surfaceRenderer.createMapDump(pos.getTileX() - mapsize / 2, pos.getTileY() - mapsize / 2, mapsize, mapsize, pos.getTileX(), pos.getTileY());
+				image = surfaceRenderer.createMapDump(px - mapsize / 2, py - mapsize / 2, mapsize, mapsize, px, py);
 			else
-				image = caveRenderer.createMapDump(pos.getTileX() - cavemapsize / 2, pos.getTileY() - cavemapsize / 2, cavemapsize, cavemapsize, pos.getTileX(), pos.getTileY());
+				image = caveRenderer.createMapDump(px - cavemapsize / 2, py - cavemapsize / 2, cavemapsize, cavemapsize, px, py);
 			texture = ImageTextureLoader.loadNowrapNearestTexture(image);
+			
+			dirty = false;
 		}
 	}
 	
@@ -96,6 +108,16 @@ public class LiveMap {
 
 			GL11.glEnd();
 		}
+	}
+
+	@Override
+	public void caveChanged(int paramInt1, int paramInt2, int paramInt3, int paramInt4, boolean paramBoolean) {
+		dirty = true;
+	}
+
+	@Override
+	public void terrainUpdated(int paramInt1, int paramInt2, int paramInt3, int paramInt4, boolean paramBoolean1, boolean paramBoolean2) {
+		dirty = false;
 	}
 
 }
