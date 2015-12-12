96659f424e2352950d35152b1f099b5138fe164f
Alexander Gottwald <alexander.gottwald@gmail.com>
2015-12-12 21:31:19 +0100

    use OpenGL constants

--- src/com/wurmonline/client/renderer/gui/LiveMapWindow.java
+++ src/com/wurmonline/client/renderer/gui/LiveMapWindow.java
@@ -29,17 +29,17 @@ public class LiveMapWindow extends WWindow {
 		super.renderComponent(alpha);
 		
 		liveMap.update(x, y);
+				
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		
-		GL11.glEnable(3553);
-		
-		GL11.glEnable(3042);
-		GL11.glBlendFunc(770, 771);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		GL11.glColor4f(r, g, b, 1.0F);
 
 		liveMap.render(0.0F, 0.0F, 1.0F);
 		
-		GL11.glDisable(3042);
-		GL11.glDisable(3553);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 	}
 	
