8e6b1f4b69d66e9df48be2e4b96003c42956e5f0
Alexander Gottwald <alexander.gottwald@gmail.com>
2017-07-26 11:16:29 +0200

    Added paved cave floors

--- src/main/java/org/gotti/wurmonline/clientmods/livehudmap/renderer/MapRendererCave.java
+++ src/main/java/org/gotti/wurmonline/clientmods/livehudmap/renderer/MapRendererCave.java
@@ -79,7 +79,7 @@ public class MapRendererCave extends AbstractCaveRenderer {
 	}
 
 	private boolean isTunnel(Tile tileType) {
-		return tileType == Tile.TILE_CAVE || tileType == Tile.TILE_CAVE_FLOOR_REINFORCED || tileType == Tile.TILE_CAVE_EXIT;
+		return tileType == Tile.TILE_CAVE || tileType == Tile.TILE_CAVE_FLOOR_REINFORCED || tileType == Tile.TILE_CAVE_EXIT || tileType == Tile.TILE_CAVE_PREPATED_FLOOR_REINFORCED;
 	}
 
 	private boolean isSurroundedByRock(int x, int y) {
